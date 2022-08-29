e25a8012905f49fde501c3ca18c7e9756c6029b1
Boris Iskrenov <91016936+Bochkata@users.noreply.github.com>
2022-08-30 00:24:36 +0300

    Add files via upload

---
+++ C# FUNDAMENTALS/Mid_Exam/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+                if (command[0] == "Include")
+                {
+                    input.Add(command[1]);
+                }  
+
+
+
+
+                if (command[0] == "Remove")
+                {
+                    if (command[1]=="first")
+                    {
+                        if (int.Parse(command[2])<=input.Count)
+                        {
+                            for (int j = 0; j < int.Parse(command[2]); j++)
+                            {
+                                input.RemoveAt(0);
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    if (command[1] == "last")
+                    {
+                        if (int.Parse(command[2]) <= input.Count)
+                        {
+                            for (int j = 0; j < int.Parse(command[2]); j++)
+                            {
+                                input.RemoveAt(input.Count-1);
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+
+
+                if (command[0]=="Prefer")
+                {
+                    if (int.Parse(command[1]) < input.Count && int.Parse(command[2]) < input.Count && int.Parse(command[1]) != int.Parse(command[2]))
+                    {
+                        string temp = input[int.Parse(command[1])];
+                        input[int.Parse(command[1])] = input[int.Parse(command[2])];
+                        input[int.Parse(command[2])] = temp;
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+
+                if (command[0] == "Reverse")
+                {
+                    input.Reverse();
+                }
+
+            }
+            Console.WriteLine("Coffees:");
+            Console.WriteLine(string.Join(" ",input));
+
+        }
+    }
+}
