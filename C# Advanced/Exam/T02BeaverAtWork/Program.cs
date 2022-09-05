374c83ceb437d5b16893425a68feb8a274ae17e1
Boris Iskrenov <91016936+Bochkata@users.noreply.github.com>
2022-09-05 10:00:25 +0300

    Update Program.cs

--- C# Advanced/Exam/T02BeaverAtWork/Program.cs
+++ C# Advanced/Exam/T02BeaverAtWork/Program.cs
@@ -1,183 +1,219 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
-namespace T02BeaverAtWork
+namespace _2
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            int size = int.Parse(Console.ReadLine());
-            char[,] matrix = new char[size, size];
-
-            int beaverRow = 0;
-            int beaverCol = 0;
-            int countOfWoods = 0;
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            int n = int.Parse(Console.ReadLine());
+            char[,] array = new char[n, n];
+            int currentRow = 0;
+            int currentCol = 0;
+            List<char> collected = new List<char>();
+            int pickedCounter=0;
+            int countToCollect = 0;
+            for (int i = 0; i < n; i++)
             {
-                char[] currRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse)
-                    .ToArray();
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int j = 0; j < n; j++)
                 {
-                    matrix[i, j] = currRow[j];
-                    if (matrix[i, j] == 'B')
+                    array[i, j] = char.Parse(input[j]);
+                    if (array[i,j]=='B')
                     {
-                        beaverRow = i;
-                        beaverCol = j;
+                        currentRow = i;
+                        currentCol = j;
+                    }
+                    if (char.IsLower(array[i,j]))
+                    {
+                        countToCollect++;
+                    }
+                }
+            }
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        Console.Write(array[i,j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            while (countToCollect>pickedCounter)
+            {
+                string command = Console.ReadLine();
+                if (command=="end")
+                {
+                    break;
+                }
+                else
+                {
+                    if (command=="up")
+                    {
+                        if (currentRow<1)
+                        {
+                            if (collected.Count>0)
+                            {
+                                collected.RemoveAt(collected.Count - 1);
+                            }
+                        }
+                        else
+                        {
+                            array[currentRow, currentCol] = '-';
+                            currentRow--;
+                            if (array[currentRow,currentCol]=='F')
+                            {
+                                if (currentRow!=0)
+                                {
+                                    array[currentRow, currentCol] = '-';
+                                    currentRow = 0;
+                                }
+                                else
+                                {
+                                    array[currentRow, currentCol] = '-';
+                                    currentRow = n - 1;
+                                }
+                            }
+                            if (char.IsLower(array[currentRow,currentCol]))
+                            {
+                                collected.Add(array[currentRow, currentCol]);
+                                pickedCounter++;
+                            }
+                            array[currentRow, currentCol] = 'B';
+                        }
                     }
 
-                    if (Char.IsLower(matrix[i, j]))
+
+
+
+                    if (command == "down")
                     {
-                        countOfWoods++;
+                        if (currentRow > n-2)
+                        {
+                            if (collected.Count > 0)
+                            {
+                                collected.RemoveAt(collected.Count - 1);
+                            }
+                        }
+                        else
+                        {
+                            array[currentRow, currentCol] = '-';
+                            currentRow++;
+                            if (array[currentRow, currentCol] == 'F')
+                            {
+                                if (currentRow != n-1)
+                                {
+                                    array[currentRow, currentCol] = '-';
+                                    currentRow = n-1;
+                                }
+                                else
+                                {
+                                    array[currentRow, currentCol] = '-';
+                                    currentRow = 0;
+                                }
+                            }
+                            if (char.IsLower(array[currentRow, currentCol]))
+                            {
+                                collected.Add(array[currentRow, currentCol]);
+                                pickedCounter++;
+                            }
+                            array[currentRow, currentCol] = 'B';
+                        }
+                    }
+
+
+
+                    if (command == "left")
+                    {
+                        if (currentCol < 1)
+                        {
+                            if (collected.Count > 0)
+                            {
+                                collected.RemoveAt(collected.Count - 1);
+                            }
+                        }
+                        else
+                        {
+                            array[currentRow, currentCol] = '-';
+                            currentCol--;
+                            if (array[currentRow, currentCol] == 'F')
+                            {
+                                if (currentCol != 0)
+                                {
+                                    array[currentRow, currentCol] = '-';
+                                    currentCol = 0;
+                                }
+                                else
+                                {
+                                    array[currentRow, currentCol] = '-';
+                                    currentCol = n - 1;
+                                }
+                            }
+                            if (char.IsLower(array[currentRow, currentCol]))
+                            {
+                                collected.Add(array[currentRow, currentCol]);
+                                pickedCounter++;
+                            }
+                            array[currentRow, currentCol] = 'B';
+                        }
+                    }
+
+
+
+                    if (command == "right")
+                    {
+                        if (currentCol > n-2)
+                        {
+                            if (collected.Count > 0)
+                            {
+                                collected.RemoveAt(collected.Count - 1);
+                            }
+                        }
+                        else
+                        {
+                            array[currentRow, currentCol] = '-';
+                            currentCol++;
+                            if (array[currentRow, currentCol] == 'F')
+                            {
+                                if (currentCol != 0)
+                                {
+                                    array[currentRow, currentCol] = '-';
+                                    currentCol = n-1;
+                                }
+                                else
+                                {
+                                    array[currentRow, currentCol] = '-';
+                                    currentCol = 0;
+                                }
+                            }
+                            if (char.IsLower(array[currentRow, currentCol]))
+                            {
+                                collected.Add(array[currentRow, currentCol]);
+                                pickedCounter++;
+                            }
+                            array[currentRow, currentCol] = 'B';
+                        }
                     }
 
                 }
             }
-
-            int initialCountOfWoods = countOfWoods;
-            string command;
-            List<char> collectedWoods = new List<char>();
-            while ((command = Console.ReadLine()) != "end")
+            if (countToCollect==pickedCounter)
             {
-                int oldBeaverRow = beaverRow;
-                int oldbeaverCol = beaverCol;
-                matrix[beaverRow, beaverCol] = '-';
-                if (command == "up")
-                {
-                    beaverRow--;
-                }
-                else if (command == "down")
-                {
-                    beaverRow++;
-                }
-                else if (command == "left")
-                {
-                    beaverCol--;
-                }
-                else if (command == "right")
-                {
-                    beaverCol++;
-                }
-
-                if (!IsWIthinMatrix(matrix, beaverRow, beaverCol))
-                {
-                    if (collectedWoods.Count > 0)
-                    {
-                        collectedWoods.RemoveAt(collectedWoods.Count - 1);
-                        initialCountOfWoods--;
-                    }
-                    beaverRow = oldBeaverRow;
-                    beaverCol = oldbeaverCol;
-                }
-                else if (IsWIthinMatrix(matrix, beaverRow, beaverCol))
-                {
-                    if (Char.IsLower(matrix[beaverRow, beaverCol]))
-                    {
-                        collectedWoods.Add(matrix[beaverRow, beaverCol]);
-                        countOfWoods--;
-                        matrix[beaverRow, beaverCol] = 'B';
-                        if (countOfWoods == 0)
-                        {
-                            break;
-                        }
-
-                    }
-                    else if (matrix[beaverRow, beaverCol] == 'F' && beaverRow != 0 && beaverCol != 0 &&
-                             beaverRow != matrix.GetLength(0) - 1 && beaverCol != matrix.GetLength(1))
-                    {
-                        matrix[beaverRow, beaverCol] = '-';
-                        MovesToEnd(matrix, command, ref beaverRow, ref beaverCol);
-                    }
-                    else if (matrix[beaverRow, beaverCol] == 'F' && (beaverRow == 0 || beaverCol == 0 ||
-                                                                     beaverRow == matrix.GetLength(0) - 1 ||
-                                                                     beaverCol == matrix.GetLength(1)))
-                    {
-                        matrix[beaverRow, beaverCol] = '-';
-                        SwimsOppositeEnd(matrix, command, ref beaverRow, ref beaverCol);
-                        if (Char.IsLetter(matrix[beaverRow, beaverCol]))
-                        {
-                            collectedWoods.Add(matrix[beaverRow, beaverCol]);
-                        }
-                    }
-
-                }
-                matrix[beaverRow, beaverCol] = 'B';
-            }
-
-            if (initialCountOfWoods == collectedWoods.Count)
-            {
-                Console.WriteLine(
-                    $"The Beaver successfully collect {collectedWoods.Count} wood branches: {string.Join(", ", collectedWoods)}.");
+                Console.WriteLine($"The Beaver successfully collect {collected.Count} wood branches: {string.Join(", ",collected)}.");
             }
             else
             {
-                Console.WriteLine(
-                    $"The Beaver failed to collect every wood branch. There are {initialCountOfWoods - collectedWoods.Count} branches left.");
+                Console.WriteLine($"The Beaver failed to collect every wood branch. There are {countToCollect-pickedCounter} branches left.");
             }
-
-            PrintMatrix(matrix);
-
-        }
-
-        private static void SwimsOppositeEnd(char[,] matrix, string command, ref int beaverRow, ref int beaverCol)
-        {
-
-            if (command == "up")
+            for (int i = 0; i < n; i++)
             {
-                beaverRow = matrix.GetLength(0) - 1;
-            }
-            else if (command == "down")
-            {
-                beaverRow = 0;
-            }
-            else if (command == "right")
-            {
-                beaverCol = 0;
-            }
-            else if (command == "left")
-            {
-                beaverCol = matrix.GetLength(1) - 1;
-            }
-        }
-
-        private static void MovesToEnd(char[,] matrix, string command, ref int beaverRow, ref int beaverCol)
-        {
-            if (command == "up")
-            {
-                beaverRow = 0;
-            }
-            else if (command == "down")
-            {
-                beaverRow = matrix.GetLength(0) - 1;
-            }
-            else if (command == "left")
-            {
-                beaverCol = 0;
-            }
-            else if (command == "right")
-            {
-                beaverCol = matrix.GetLength(1) - 1;
-            }
-        }
-
-        public static bool IsWIthinMatrix(char[,] matrix, int row, int col)
-            => row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
-
-        public static void PrintMatrix(char[,] matrix)
-        {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < n; j++)
                 {
-                    Console.Write(matrix[i, j] + " ");
+                    Console.Write(array[i, j]+" ");
                 }
-
                 Console.WriteLine();
             }
-
         }
     }
 }
