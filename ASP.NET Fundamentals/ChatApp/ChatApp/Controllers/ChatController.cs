8d642e4c70b7cb741de943fc137558b143ba51a0
Boris Iskrenov <91016936+Bochkata@users.noreply.github.com>
2022-10-25 17:06:09 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,44 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> Messages = new List<KeyValuePair<string, string>>();
+
+        
+        public IActionResult Show()
+        {
+            if(Messages.Count < 1)
+            {
+                return View(new ChatViewModel());  
+            }
+
+            ChatViewModel chatModel = new ChatViewModel()
+            {
+                Messages = Messages.Select(x => new MessageViewModel()
+                {
+                    Sender = x.Key,
+                    MessageText = x.Value
+                })
+                .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            MessageViewModel newMessage = chat.CurrentMessage;
+            Messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+
+            return RedirectToAction("Show");
+        }
+
+
+
+
+    }
+}
---
+++ ASP.NET Fundamentals/ChatApp/ChatApp/Models/MessageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Models
+{
+    public class MessageViewModel
+    {
+        public string Sender { get; set; }  
+        public string MessageText { get; set; } 
+
+
+    }
+}
