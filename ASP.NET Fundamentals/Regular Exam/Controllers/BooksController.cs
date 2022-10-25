23d7197aae5c329c8c498709ff5b8dc199e994eb
Boris Iskrenov <91016936+Bochkata@users.noreply.github.com>
2022-10-25 17:42:21 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Regular Exam/Controllers/BooksController.cs
@@ -0,0 +1,88 @@
+﻿using Library.Contracts;
+using Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Library.Controllers
+{
+    [Authorize]
+    public class BooksController : Controller
+    {
+       
+        private readonly IBook bookService;
+
+        public BooksController(IBook _bookService)
+        {
+            bookService = _bookService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await bookService.GetAllAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new AddBookViewModel()
+            {
+                Categories = await bookService.GetCategoryAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBookViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            try
+            {
+                await bookService.Add(model);
+                return RedirectToAction(nameof(All));
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Something went wrong");
+                return View(model);
+            }
+        }
+
+        public async Task<IActionResult> AddToCollection(int bookId)
+        {
+            try
+            {
+                var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+                await bookService.AddBookToCollectionAsync(bookId, userId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return RedirectToAction(nameof(All));
+        }
+
+        public async Task<IActionResult> Read()
+        {
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            var model = await bookService.GetMyBooksAsync(userId);
+
+            return View("Mine", model);
+        }
+
+        public async Task<IActionResult> RemoveFromCollection(int bookId)
+        {
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            await bookService.RemoveBookFromCollectionAsync(bookId, userId);
+            
+            return RedirectToAction(nameof(Read));
+        }
+    }
+}
