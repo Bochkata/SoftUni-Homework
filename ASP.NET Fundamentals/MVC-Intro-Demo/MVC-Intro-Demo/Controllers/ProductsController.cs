54d02f016bca4c3b34e623d12c2d8e008f6b5012
Boris Iskrenov <91016936+Bochkata@users.noreply.github.com>
2022-10-25 17:10:45 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/MVC-Intro-Demo/MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Json;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id=2,
+                Name = "Ham",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id=3,
+                Name = "Bread",
+                Price = 1.50
+
+            }
+        };
+
+        [ActionName("My-Products")]
+        public IActionResult All(string keyword)
+        {
+            if (keyword != null)
+            {
+                IEnumerable<ProductViewModel> foundProduct = products.Where(x => x.Name.ToLower().Contains(keyword.ToLower()));
+
+                return View(foundProduct);
+            }
+           return View(products);   
+
+        }
+        //public IActionResult All()
+        //{
+        //    return View(this.products);
+        //}
+
+        public IActionResult ById(int Id)
+        {
+            ProductViewModel product = products.FirstOrDefault(x => x.Id == Id);
+
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            return View(product);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            return Json(products, options);
+        }
+
+        public IActionResult AllAsText()
+        {
+            StringBuilder text = new StringBuilder();
+            foreach (ProductViewModel product in products)
+            {
+                text.AppendLine($"Product {product.Id}: {product.Name} - {product.Price}lv");
+            }
+            return Content(text.ToString());
+        }
+
+        public IActionResult AllAsTextFile()
+        {
+            StringBuilder text = new StringBuilder();
+            foreach (ProductViewModel product in products)
+            {
+                text.AppendLine($"Product {product.Id}: {product.Name} - {product.Price}lv");
+            }
+
+            Response.Headers.Add("content-disposition", "attachment; filename=products.txt");
+
+            return File(Encoding.UTF8.GetBytes(text.ToString()), "text/plain");
+
+            //return File(Encoding.UTF8.GetBytes(text.ToString()), "text/plain", "products.txt");
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
